cea9ae0452023ceff92845e239ec8aed38a79538
Maya010505 <mayushka0105@yandex.ru>
2025-06-11 18:56:58 +0300

    commited

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,7 +6,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts/create/", views.post_edit, name="post_create"),
+    path("posts/create/", views.post_edit, name="create_post"),
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
     path("posts/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("posts/<int:post_id>/delete/", views.post_delete, name="post_delete"),
